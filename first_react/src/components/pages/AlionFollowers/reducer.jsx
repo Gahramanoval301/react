d13e7b72efbc8f0629a494e07ada0384075d9a03
Gahramanoval301 <lemangahramanoval@gmail.com>
2023-11-03 22:09:17 +0300

    remove 1st element if the 1st letter starts with a

--- first_react/src/components/pages/AlionFollowers/reducer.jsx
+++ first_react/src/components/pages/AlionFollowers/reducer.jsx
@@ -7,17 +7,26 @@ export const initialState = {
     count: 0,
     followers: []
 }
-export const actions = {
+export let actions = {
     'get_followers': 0,
-    'remove_follower': 1
+    'remove_follower': 1,
+    'rm_remove_with_a': 2
 }
 const reducerFollowers = (state = initialState, action) => {
     switch (action.type) {
         case actions.get_followers:
             return { ...state, followers: action.payload }
         case actions.remove_follower:
-            return { ...state, followers: state.followers.filter((follower) =>
-                    follower.id !== action.payload )}
+            return {
+                ...state, followers: state.followers.filter((follower) =>
+                    follower.id !== action.payload)
+            }
+        case actions.rm_remove_with_a:
+            const index = state.followers.findIndex((fol) => fol.login.toLowerCase().startsWith('a'))
+            console.log(index)
+            const fols = [...state.followers]
+            if (index !== -1) fols.splice(index, 1)
+            return { ...state, followers: fols }
         default:
             return state;
     }
