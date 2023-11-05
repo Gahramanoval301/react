323f72dafcccfd4f4d9c8775f738503fcd808042
Gahramanoval301 <lemangahramanoval@gmail.com>
2023-11-05 14:57:13 +0300

    delete and add contacts with axios

--- first_react/src/components/pages/ContactWithRedux/index.jsx
+++ first_react/src/components/pages/ContactWithRedux/index.jsx
@@ -5,12 +5,42 @@ import PageContainer from '../../PageContainer'
 
 const _url = 'http://localhost:3000/contacts'
 
+const imgStyle = {
+    width: '128px',
+    height: '128px',
+    objectFit: 'cover',
+    objectPosition: 'center'
+}
+const deleteBtnStyle = {
+    border: '1px solid navy',
+    borderRadius: '5px',
+    padding: '5px',
+    backgroundColor: 'aliceblue',
+    margin: '5px 0px',
+    cursor: 'pointer',
+}
+const buttonStyle = {
+    cursor: 'pointer',
+    backgroundColor: 'navy',
+    color: 'white',
+    fontWeigth: 'bold',
+    fontFamily: 'monospace',
+    border: 'none',
+    borderRadius: '10px',
+    boxShadow: '0 0 10px gray',
+    padding: '10px 15px',
+    margin: '1.2rem'
+}
 const contactCardStyle = {
     border: '1px solid',
     maxWidth: '20vw',
     padding: '5px 10px',
-    boxShadow: '0 0 10px gray'
-    }
+    boxShadow: '0 0 10px gray',
+    display: 'flex',
+    flexFlow: 'column wrap',
+    justifyContent: 'start',
+    alignItems: 'start'
+}
 const containerStyle = {
     display: 'flex',
     gap: '1.5rem',
@@ -20,18 +50,23 @@ const containerStyle = {
 const ContactsWithRedux = ({ contacts, dispatch }) => {
     useEffect(() => {
         axios.get(_url).then(({ data }) => { dispatch({ type: 'get_contacts', payload: data }) })
-    }, [])
-    console.log('conytacts', contacts);
+    }, [contacts])
     return (
         <PageContainer>
+            <button style={buttonStyle} onClick={() => dispatch({ type: 'add_contact' })} >add contact</button>
             <div style={containerStyle}>
                 {
                     contacts.map(({ id, age, name, photo }) => {
                         return (
                             <div key={id} style={contactCardStyle}>
                                 <p>{name} <span>{age}</span></p>
-                                <img src={photo} alt={`${name}'s photo`} />
-
+                                <img style={imgStyle} src={photo} alt={`${name}'s photo`} />
+                                <button style={deleteBtnStyle} onClick={() => {
+                                    dispatch({
+                                        type: 'delete_contact',
+                                        payload: id
+                                    })
+                                }}>❌</button>
                             </div>
                         )
                     })
--- first_react/src/components/redux/index.jsx
+++ first_react/src/components/redux/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-
+import axios from 'axios'
+const url = 'http://localhost:3000/contacts'
 const initialStore = {
     count: 0,
     users: [],
     contacts: []
 }
+
 const RootReducer = (store = initialStore, action) => {
     switch (action.type) {
         case 'inc':
@@ -28,6 +30,14 @@ const RootReducer = (store = initialStore, action) => {
             return { ...store, users: rm_last_user }
         case 'get_contacts':
             return { ...store, contacts: action.payload }
+        case 'add_contact':
+            let idc = store.contacts.length + 1;
+            let name = prompt('Please enter name:')
+            let age = prompt('Please enter age:')
+            let photo = prompt('Please enter image url:')
+            axios.post(url, { id: idc, name, age, photo })
+        case 'delete_contact':
+            axios.delete(url + '/' + action.payload)
         default:
             return store;
 
