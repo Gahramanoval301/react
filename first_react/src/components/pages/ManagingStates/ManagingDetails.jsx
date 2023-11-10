12a9f187528ae9033c71ee41c6c0a69baef83c3d
Gahramanoval301 <lemangahramanoval@gmail.com>
2023-11-10 17:10:19 +0300

    inputWork3 is done

--- first_react/src/components/pages/ManagingStates/ManagingDetails.jsx
+++ first_react/src/components/pages/ManagingStates/ManagingDetails.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { useParams } from 'react-router';
 import InputWork1 from './inputWork1';
 import InputWork2 from './inputWork2';
+import InputWork3 from './inputWork3';
 
 const ManagingDetails = () => {
     const {inputW} = useParams()
     if (inputW === 'inputwork1') return <InputWork1/>
     else if (inputW === 'inputwork2') return <InputWork2/>
+    else if (inputW === 'inputwork3') return <InputWork3/>
 }
 
 export default ManagingDetails
---
+++ first_react/src/components/pages/ManagingStates/inputWork3/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const inputWork3 = () => {
+  return (
+    <div>inputWork3</div>
+  )
+}
+
+export default inputWork3
