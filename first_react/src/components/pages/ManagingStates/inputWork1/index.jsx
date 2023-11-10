3152fac78f3e5148c2c058985294e754cbeb08e1
Gahramanoval301 <lemangahramanoval@gmail.com>
2023-11-10 16:05:53 +0300

    inputwork1 is done

--- first_react/src/components/pages/ManagingStates/inputWork1/index.jsx
+++ first_react/src/components/pages/ManagingStates/inputWork1/index.jsx
@@ -1,9 +1,64 @@
 import React from 'react'
-
+import { useState } from 'react'
+import './index.css'
 const InputWork1 = () => {
-  return (
-    <div>InputWork1</div>
-  )
+    // Create states
+    let [error, setError] = useState(null)
+    let [answer, setAnswer] = useState('')
+    let [status, setStatus] = useState('typing') //typing, submitting, success
+
+    if (status === 'success') return <h1>That's right. You are is JS developer!</h1>
+
+    function handleTextareaChange(e) {
+        setAnswer(e.target.value)
+    }
+
+    async function handleSubmit(e) {
+        e.preventDefault()
+        setStatus('submitting')
+        try {
+            await SubmitForm(answer)
+            setStatus('success')
+        } catch (error) {
+            setStatus('typing')
+            setError(error.message)
+        }
+    }
+
+    async function SubmitForm(answer) {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                let shouldError = (answer !== '1995')
+                if (shouldError) {
+                    reject(new Error('good guess but false answer, try again!'))
+                }
+                else {
+                    resolve()
+                }
+            }, 1500);
+        })
+
+    }
+
+    return (
+        <div className='form'>
+            <h2>City quiz</h2>
+            <p>Which year JavaScript is created?</p>
+            <form onSubmit={handleSubmit}>
+                <textarea name="answer" id="answer"
+                    value={answer}
+                    onChange={handleTextareaChange}
+                    disabled={status === 'submitting'}
+                />
+                <button className='submit'
+                    disabled={status === 'submitting' || answer.length === 0}
+                >Submit</button>
+                {
+                    error !== null && <p className='error'>{error}</p>
+                }
+            </form>
+        </div>
+    )
 }
 
 export default InputWork1
--- first_react/src/components/pages/ManagingStates/inputWork2/index.jsx
+++ first_react/src/components/pages/ManagingStates/inputWork2/index.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 
 const InputWork2 = () => {
-  return (
-    <div>InputWork2</div>
-  )
+   let promise = new Promise(function(resolve, reject){
+        resolve('Im done')
+        reject(new Error('im rejected'))
+    })
+    console.log(promise);
+    promise.then((obj)=>{
+        console.log(obj);
+    })
+    .catch((obj)=>{
+        console.log(obj);
+    })
+ 
 }
 
 export default InputWork2
